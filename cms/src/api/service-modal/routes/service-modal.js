7015ce815a9f4211b942d32d81424ea1f59fa5b0
Ngu DUONG <nguduong1988@gmail.com>
2022-11-28 08:34:34 +0700

    add service modal

---
+++ cms/src/api/service-modal/routes/service-modal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * service-modal router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::service-modal.service-modal');
---
+++ cms/src/api/service-modal/services/service-modal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * service-modal service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::service-modal.service-modal');
